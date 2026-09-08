f33c06293b8525af86ebf922407c4c14a651bd9d
Sly <sly@github.com>
2026-09-08 16:50:06 +0330

    Fix compose uiToolingPreview catalog reference and remove deprecated android newDsl flag

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
             implementation(libs.compose.material3)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
-            implementation(libs.compose.components.uiToolingPreview)
+            implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.androidx.datastore.preferences)
